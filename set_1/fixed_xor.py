91f742b8ef8b060fef73092b315b2c4eb4fc7d5f
Tom Yue <yuethomas@gmail.com>
2019-04-21 12:16:09 -0700

    Drop old files

--- set_1/fixed_xor.py
+++
@@ -1,9 +0,0 @@
-"""S1C2: take two equal length buffers and produces their XOR."""
-
-
-def fixed_xor(bytearray1, bytearray2):
-  """Takes two equal length bytearrays and produces their XOR."""
-
-  return bytearray([
-      byte1 ^ byte2 for (byte1, byte2)
-      in zip(bytearray1, bytearray2)])
--- set_1/repeating_key_xor_cipher.py
+++
@@ -1,9 +0,0 @@
-"""S1C5. Repeating-key XOR."""
-
-def repeating_key_xor_cipher(plaintext, cipher):
-  """Encodes plaintext with cipher. Both inputs are bytearrays."""
-
-  return bytearray([
-      plaintext[i] ^ cipher[i % len(cipher)]
-      for i in range(len(plaintext))
-  ])
